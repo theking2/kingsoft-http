3d1864593d76c19c8c29790a0516d8c909af0190
Johannes <theking2@king.ma>
2024-01-22 15:29:38 +0000

    Add sample implementation of the abstract Rest

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+define( 'ROOT', $_SERVER['DOCUMENT_ROOT'] . '/' );
+require ROOT . 'config.inc.php';
+
+require 'RestImplementation.class.php';
+
+use Kingsoft\Http\Request;
+
+
+$api = new SbwMonitor( new Request(
+  SETTINGS['api']['allowedendpoints'],
+  SETTINGS['api']['allowedmethods'] ?? 'OPTIONS,HEAD,GET,POST,PUT,DELETE',
+  SETTINGS['api']['allowedorigin'] ?? '*',
+  (int) SETTINGS['api']['maxage']
+) );
