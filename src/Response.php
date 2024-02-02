a4a13e869d281cc906f607940f4ccf89f5237536
Johannes Kingma <j.kingma@sbw.edu>
2024-02-02 08:47:43 +0100

    Move StatusCode toString to StatusCode Fixes #26

--- src/Response.php
+++ src/Response.php
@@ -22,59 +22,6 @@ class Response
 {
 
 	/**
-	 * decodeHttpResponse
-	 *
-	 * @param  mixed $statusCode
-	 * @return string
-	 */
-	private static function decodeHttpResponse( StatusCode $statusCode ): string
-	{
-		return match ( $statusCode ) {
-			StatusCode::Continue => 'Continue',
-			StatusCode::SwitchingProtocols => 'Switching Protocols',
-			StatusCode::OK => 'OK',
-			StatusCode::Created => 'Created',
-			StatusCode::Accepted => 'Accepted',
-			StatusCode::NonAuthoritativeInformation => 'Non-Authoritative Information',
-			StatusCode::NoContent => 'No Content',
-			StatusCode::ResetContent => 'Reset Content',
-			StatusCode::PartialContent => 'Partial Content',
-			StatusCode::MultipleChoices => 'Multiple Choices',
-			StatusCode::MovedPermanently => 'Moved Permanently',
-			StatusCode::Found => 'Found',
-			StatusCode::SeeOther => 'See Other',
-			StatusCode::NotModified => 'Not Modified',
-			StatusCode::UseProxy => 'Use Proxy',
-			StatusCode::TemporaryRedirect => 'Temporary Redirect',
-			StatusCode::BadRequest => 'Bad Request',
-			StatusCode::Unauthorized => 'Unauthorized',
-			StatusCode::PaymentRequired => 'Payment Required',
-			StatusCode::Forbidden => 'Forbidden',
-			StatusCode::NotFound => 'Not Found',
-			StatusCode::MethodNotAllowed => 'Method Not Allowed',
-			StatusCode::NotAcceptable => 'Not Acceptable',
-			StatusCode::ProxyAuthenticationRequired => 'Proxy Authentication Required',
-			StatusCode::RequestTimeout => 'Request Timeout',
-			StatusCode::Conflict => 'Conflict',
-			StatusCode::Gone => 'Gone',
-			StatusCode::LengthRequired => 'Length Required',
-			StatusCode::PreconditionFailed => 'Precondition Failed',
-			StatusCode::RequestEntityTooLarge => 'Request Entity Too Large',
-			StatusCode::RequestURITooLong => 'Request-URI Too Long',
-			StatusCode::UnsupportedMediaType => 'Unsupported Media Type',
-			StatusCode::RequestedRangeNotSatisfiable => 'Requested Range Not Satisfiable',
-			StatusCode::ExpectationFailed => 'Expectation Failed',
-			StatusCode::InternalServerError => 'Internal Server Error',
-			StatusCode::NotImplemented => 'Not Implemented',
-			StatusCode::BadGateway => 'Bad Gateway',
-			StatusCode::ServiceUnavailable => 'Service Unavailable',
-			StatusCode::GatewayTimeout => 'Gateway Timeout',
-			StatusCode::HTTPVersionNotSupported => 'HTTP Version Not Supported',
-			default => 'Unknown HTTP status code'
-		};
-	}
-
-	/**
 	 * sendStatusCode
 	 *
 	 * @param  mixed $statusCode
--- src/StatusCode.php
+++ src/StatusCode.php
@@ -44,5 +44,60 @@ enum StatusCode: int
 	case ServiceUnavailable = 503;
 	case GatewayTimeout = 504;
 	case HTTPVersionNotSupported = 505;
+
+	
+	/**
+	 * toString
+	 * Current versions of PHP don't accept a magic function here
+	 *
+	 * @param  mixed $statusCode
+	 * @return string
+	 */
+	private static function toString( StatusCode $statusCode ): string
+	{
+		return match ( $statusCode ) {
+			StatusCode::Continue => 'Continue',
+			StatusCode::SwitchingProtocols => 'Switching Protocols',
+			StatusCode::OK => 'OK',
+			StatusCode::Created => 'Created',
+			StatusCode::Accepted => 'Accepted',
+			StatusCode::NonAuthoritativeInformation => 'Non-Authoritative Information',
+			StatusCode::NoContent => 'No Content',
+			StatusCode::ResetContent => 'Reset Content',
+			StatusCode::PartialContent => 'Partial Content',
+			StatusCode::MultipleChoices => 'Multiple Choices',
+			StatusCode::MovedPermanently => 'Moved Permanently',
+			StatusCode::Found => 'Found',
+			StatusCode::SeeOther => 'See Other',
+			StatusCode::NotModified => 'Not Modified',
+			StatusCode::UseProxy => 'Use Proxy',
+			StatusCode::TemporaryRedirect => 'Temporary Redirect',
+			StatusCode::BadRequest => 'Bad Request',
+			StatusCode::Unauthorized => 'Unauthorized',
+			StatusCode::PaymentRequired => 'Payment Required',
+			StatusCode::Forbidden => 'Forbidden',
+			StatusCode::NotFound => 'Not Found',
+			StatusCode::MethodNotAllowed => 'Method Not Allowed',
+			StatusCode::NotAcceptable => 'Not Acceptable',
+			StatusCode::ProxyAuthenticationRequired => 'Proxy Authentication Required',
+			StatusCode::RequestTimeout => 'Request Timeout',
+			StatusCode::Conflict => 'Conflict',
+			StatusCode::Gone => 'Gone',
+			StatusCode::LengthRequired => 'Length Required',
+			StatusCode::PreconditionFailed => 'Precondition Failed',
+			StatusCode::RequestEntityTooLarge => 'Request Entity Too Large',
+			StatusCode::RequestURITooLong => 'Request-URI Too Long',
+			StatusCode::UnsupportedMediaType => 'Unsupported Media Type',
+			StatusCode::RequestedRangeNotSatisfiable => 'Requested Range Not Satisfiable',
+			StatusCode::ExpectationFailed => 'Expectation Failed',
+			StatusCode::InternalServerError => 'Internal Server Error',
+			StatusCode::NotImplemented => 'Not Implemented',
+			StatusCode::BadGateway => 'Bad Gateway',
+			StatusCode::ServiceUnavailable => 'Service Unavailable',
+			StatusCode::GatewayTimeout => 'Gateway Timeout',
+			StatusCode::HTTPVersionNotSupported => 'HTTP Version Not Supported',
+			default => 'Unknown HTTP status code'
+		};
+	}
+
 }
-;
